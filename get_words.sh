ce171097d6ce0a651f15fd01e6a4e047dec91592
Drahlous <MattTStachowiak@gmail.com>
2022-02-03 09:28:25 -0700

    Cleanup get_words script

--- get_words.sh
+++ get_words.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 # Get the list of all 5 letter words
-cat /usr/share/dict/words   |
-    # Remove everything except lowercase ascii characters a-z
-    sed 's/[^a-z]//g'       |
-    # Remove words < 4 characters
-    sed -E 's/^\w{,4}$//g'  |
-    # Remove words > 5 characters
-    sed -E 's/^\w{6,}$//g'  |
-    # Remove emtpy lines
-    awk NF
+cat /usr/share/dict/words | grep -E '^[a-z]{5}$'
